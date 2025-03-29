cb64c6341ed713458fe7bc27e5297885d2c2dc31
Damyan Myashkov <damyan.myashkov@gmail.com>
2025-03-29 09:01:19 +0200

    .

--- Client/src/components/ManeuverNode.tsx
+++ Client/src/components/ManeuverNode.tsx
@@ -105,7 +105,7 @@ export function ManeuverNode({
       groupRef.current.scale.setScalar(lerped);
     }
   });
-  
+
   const handlePullerDragStart = (
     event: ThreeEvent<PointerEvent>,
     direction: Direction,
